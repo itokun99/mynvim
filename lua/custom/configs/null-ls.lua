00a9487318239bed4603f73286ba56d7abb2c8cf
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2023-12-05 10:04:17 +0700

    update lua config

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -12,43 +12,47 @@ local augroup = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = fals
 local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
 
--- local sources = {
---
---   -- webdev stuff
---   -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
---   -- b.formatting.prettier.with {
---   --   filetypes = {
---   --     "html",
---   --     "markdown",
---   --     "css",
---   --     "javascript",
---   --     "typescript",
---   --     "javascriptreact",
---   --     "typescriptreact",
---   --     "json",
---   --     "scss",
---   --     "less",
---   --   },
---   -- }, -- so prettier works only on these filetypes
---
---   -- Lua
---   b.formatting.stylua,
---   -- b.diagnostics.eslint.with({
---   --   diagnostics_format = '[eslint] #{m}\n(#{c})'
---   -- }),
---   -- b.formatting.eslint,
---   -- b.diagnostics.eslint,
---   -- b.code_actions.eslint_d,
---   b.formatting.stylelint,
---   b.diagnostics.stylelint,
---
---   -- cpp
---   -- b.formatting.clang_format,
--- }
+local sources = {
+
+  -- webdev stuff
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  -- b.formatting.prettier.with {
+  --   filetypes = {
+  --     "html",
+  --     "markdown",
+  --     "css",
+  --     "javascript",
+  --     "typescript",
+  --     "javascriptreact",
+  --     "typescriptreact",
+  --     "json",
+  --     "scss",
+  --     "less",
+  --   },
+  -- }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+  -- b.diagnostics.eslint.with({
+  --   diagnostics_format = '[eslint] #{m}\n(#{c})'
+  -- }),
+  -- b.formatting.eslint,
+  -- b.diagnostics.eslint,
+  -- b.code_actions.eslint_d,
+  b.formatting.stylelint,
+  b.diagnostics.stylelint,
+
+  b.formatting.gofumpt,
+  b.formatting.goimports,
+  b.diagnostics.golangci_lint,
+
+  -- cpp
+  -- b.formatting.clang_format,
+}
 
 null_ls.setup({
   debug = true,
-  -- sources = sources,
+  sources = sources,
   on_attach = function(client, bufnr)
     if client.supports_method "textDocument/formatting" then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
