ff9862e22cbd1be0e5545eba7b6c633d5416cd71
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2023-12-20 21:57:57 +0700

    update my config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -80,6 +80,30 @@ local plugins = {
       return conf
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = true,
+    event = "VeryLazy",
+    config = function()
+      require "custom.configs.tc"
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
